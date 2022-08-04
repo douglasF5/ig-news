39bc72f9239f8ecc571491fb06d69c55c15a4a2a
Douglas Ferreira <dfaferreira46@gmail.com>
2022-08-03 21:56:10 -0300

    chore: added sass utilities

---
+++ pages/_document.tsx
@@ -0,0 +1,24 @@
+import Document, { Html, Head, Main, NextScript } from "next/document"
+
+export default class MyDocument extends Document {
+    render() {
+        return (
+            <Html lang="en">
+                <Head>
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+                    <meta name="author" content="Douglas Ferreira" />
+                    <meta name="robots" content="noindex, nofollow" />
+                    <link rel="icon" type="image/png" href="./favicon.png" />
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+                    <link href="https://fonts.googleapis.com/css2?family=Libre+Franklin:wght@400;500;600;700&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
