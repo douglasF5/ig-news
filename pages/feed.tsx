180636b07a1474acf704abc2cbf5cc381bded326
Douglas Ferreira <dfaferreira46@gmail.com>
2022-08-05 15:51:59 -0300

    layout dev: feed page

---
+++ pages/feed.tsx
@@ -0,0 +1,108 @@
+import s from '../src/styles/feed.module.scss';
+import { Header } from "../src/components/Header";
+import { Filter, X, Bookmark, BookmarkFilled } from '../src/components/Icons';
+import { useState } from 'react';
+
+export default function Feed() {
+    const postsData = [
+        {
+            title: 'Creating a Monorepo with Lerna & Yarn Workspaces',
+            publishDate: 'Mar 12, 2021',
+            preview: 'In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared build, test, and release process.'
+        },
+        {
+            title: 'Creating a Monorepo with Lerna & Yarn Workspaces',
+            publishDate: 'Mar 12, 2021',
+            preview: 'In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared build, test, and release process.'
+        },
+        {
+            title: 'Creating a Monorepo with Lerna & Yarn Workspaces',
+            publishDate: 'Mar 12, 2021',
+            preview: 'In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared build, test, and release process.'
+        },
+        {
+            title: 'Creating a Monorepo with Lerna & Yarn Workspaces',
+            publishDate: 'Mar 12, 2021',
+            preview: 'In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared build, test, and release process.'
+        },
+        {
+            title: 'Creating a Monorepo with Lerna & Yarn Workspaces',
+            publishDate: 'Mar 12, 2021',
+            preview: 'In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared build, test, and release process.'
+        },
+        {
+            title: 'Creating a Monorepo with Lerna & Yarn Workspaces',
+            publishDate: 'Mar 12, 2021',
+            preview: 'In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared build, test, and release process.'
+        }
+    ];
+    const [inputContent, setInputContent] = useState('');
+    const [bookmarkedPosts, setBookmarkedPosts] = useState<number[]>([]);
+
+    function handleClearField() {
+        setInputContent('');
+    }
+
+    function handleBookmark(postIndex: number) {
+        if(bookmarkedPosts.includes(postIndex)) {
+            let newList = bookmarkedPosts.filter(idx => idx !== postIndex);
+            setBookmarkedPosts(newList);
+        } else {
+            setBookmarkedPosts([...bookmarkedPosts, postIndex]);
+        }
+
+    }
+
+    //RETURNING STATEMENT
+    return (
+        <>
+            <Header />
+            <main className={s.sectionContainer}>
+                <div className={s.contentContainer}>
+                    <div className={s.postsTitleBar}>
+                        <h1>Feed</h1>
+                        <div className={s.inputFieldWrapper}>
+                            <Filter color='var(--c-on-surface-primary)' width={24} height={24} />
+                            <input
+                                type='text'
+                                placeholder='Type to filter'
+                                value={inputContent}
+                                onChange={(e) => setInputContent(e.target.value)}
+                            />
+                            {inputContent && (
+                                <button className={s.clearButton} onClick={handleClearField}>
+                                    <span>Clear filter</span>
+                                    <X color='var(--c-on-surface-primary)' width={24} height={24}/>
+                                </button>
+                            )}
+                        </div>
+                    </div>
+                    <div className={s.postsList}>
+                        {postsData.map(({title, publishDate, preview}, idx) => (
+                            <article key={idx} className={s.postWrapper}>
+                                <div className={s.leftWing}>
+                                    <time>{publishDate}</time>
+                                    <h2>{title}</h2>
+                                    <p>{preview}</p>
+                                </div>
+                                <button className={s.bookmarkButton} onClick={() => handleBookmark(idx)}>
+                                    <span>Bookmark post</span>
+                                    {bookmarkedPosts.includes(idx)
+                                        ? (
+                                            <BookmarkFilled
+                                                color='var(--c-green-accent-primary)'
+                                                width={20}
+                                                height={20}
+                                            />
+                                        )
+                                        : <Bookmark width={20} height={20}/>
+                                    }
+                                </button>
+                            </article>
+                        ))}
+                    </div>
+                </div>
+            </main>
+        </>
+    );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,30 +6,41 @@ import { Header } from '../src/components/Header';
 import { ChevronLeft } from '../src/components/Icons';
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Home | IgNews</title>
-      </Head>
-      <Header />
-        <main className={s.sectionContainer}>
-          <div className={s.illustration}>
-            <Image src={Illustration} alt='Illustration' />
-          </div>
-          <div className={s.contentContainer}>
-            <h1>News about the <span>React</span> world</h1>
-            <p>Get acess to all publications <span>for $9,90 month</span></p>
-            <div className={s.buttonWrapper}>
-              <button className={s.subscribeButton}>Subscribe now</button>
-              <button className={s.secondaryButton}>
-                Check out the feed
-                <span className={s.iconWrapper}>
-                  <ChevronLeft color='var(--c-on-surface-primary)' width={24} height={24} />
-                </span>
-              </button>
-            </div>
-          </div>
-        </main>
-    </>
-  )
+    return (
+        <>
+            <Head>
+                <title>Home | IgNews</title>
+            </Head>
+            <Header />
+            <main className={s.sectionContainer}>
+                <div className={s.illustration}>
+                    <Image src={Illustration} alt="Illustration" />
+                </div>
+                <div className={s.contentContainer}>
+                    <h1>
+                        News about the <span>React</span> world
+                    </h1>
+                    <p>
+                        Get acess to all publications{" "}
+                        <span>for $9,90 month</span>
+                    </p>
+                    <div className={s.buttonWrapper}>
+                        <button className={s.subscribeButton}>
+                            Subscribe now
+                        </button>
+                        <button className={s.secondaryButton}>
+                            Check out the feed
+                            <span className={s.iconWrapper}>
+                                <ChevronLeft
+                                    color="var(--c-on-surface-primary)"
+                                    width={24}
+                                    height={24}
+                                />
+                            </span>
+                        </button>
+                    </div>
+                </div>
+            </main>
+        </>
+    );
 }
